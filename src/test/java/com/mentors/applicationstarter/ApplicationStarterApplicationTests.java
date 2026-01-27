303e31c00ca777fed79b70123145d2ada7abca07
lrygl <lubomir.rygl@gmail.com>
2026-01-27 16:21:58 +0100

    Updated test configuration with build.gradle and application-test.yml

--- src/test/java/com/mentors/applicationstarter/ApplicationStarterApplicationTests.java
+++ src/test/java/com/mentors/applicationstarter/ApplicationStarterApplicationTests.java
@@ -2,8 +2,16 @@ package com.mentors.applicationstarter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.autoconfigure.exclude=" +
+				"org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration"
+})
 class ApplicationStarterApplicationTests {
 
 	@Test
